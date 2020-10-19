5605302cbbac7639b71fd52beeb8b75b8006a17a
Peter Charlton <peterhcharlton@gmail.com>
2020-10-19 10:40:31 +0100

    Update README.m

--- ppg-diary_v1.0/README.m
+++ ppg-diary_v1.0/README.m
@@ -1,1 +1,20 @@
-abc
+# PPG Diary Scripts: v.1
+
+This version of the toolbox contains the scripts used in the following publication:
+
+Charlton P.H. *et al.* **Acquiring wearable photoplethysmography data in daily life: the PPG Diary Pilot Study **, in *Engineering Proceedings*, [Under Review]
+
+The scripts are provided in Matlab &reg; format.
+
+## Summary of Publication
+
+This pilot study investigated the feasibility of acquiring photoplethysmography (PPG) data in daily living. Several lessons were learnt about how to acquire PPG data in future studies.
+
+## Replicating this Publication
+
+The work presented in this case study can be replicated as follows:
+
+*   Download the *ppg_diary_pilot_conv_data.mat* data file.
+*   Download Version 1 of the scripts
+*   Analyse the data using the *ppg_diary_pilot1_analysis.m* script. This script calls the *PulseAnalyse.m* script, so you will need this script too.
+
